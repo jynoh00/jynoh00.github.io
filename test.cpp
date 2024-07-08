e9c4e792eeb1f6abe01d3496adf8e8e246f70265
jynoh00 <wndus123sh@naver.com>
2024-07-09 05:43:33 +0900

    linkedList_plus

--- test.cpp
+++ test.cpp
@@ -1,27 +1,62 @@
 #include <iostream>
-#include <vector>
 using namespace std;
+class Node{
+private:
+    int data;
+    Node* link;
+public:
+    Node(int d = 0){ data = d; }
+    ~Node(){}
+    void setData(int d){ data = d; }
+    int getData(){ return data; }
+    void setLink(Node* nextNode){ link = nextNode; }
+    Node* getLink(){ return link; }
+};
 
-void test1(vector<int> v){
-    v.push_back(1);
-    cout << v.size() << endl;
-}
-
-void test2(vector<int>* v){
-    v->push_back(2);
-}
-
-void test3(vector<int>& v){
-    v.push_back(3);
-}
+class LinkedQueue{
+private:
+    Node* front;
+    Node* rear;
+public:
+    LinkedQueue(){ front = rear = nullptr; }
+    void enqueue(Node* n){
+        if (front == nullptr && rear == nullptr){
+            front = n;
+            rear = n;
+        }else{
+            rear->setLink(n);
+            rear = n;
+        }
+    }
+    Node* dequeue(){
+        Node* tmp = front;
+        front = front->getLink();
+        return tmp;
+    }
+    void display(){
+        Node* tmp = front;
+        while(tmp != nullptr){
+            cout << tmp->getData() << " ";
+            tmp = tmp->getLink();
+        }
+        cout << endl;
+    }
+};
 
 int main(){
-    vector<int> vector = {0, 0};
-    test1(vector);
-    test2(&vector);
-    test3(vector);
-    for (int i : vector){
-        cout << i << " ";
-    }
-    cout << endl;
+    LinkedQueue lq;
+    lq.enqueue(new Node(10));
+    lq.enqueue(new Node(20));
+    lq.enqueue(new Node(30));
+    lq.enqueue(new Node(40));
+
+    lq.display();
+
+    cout << lq.dequeue()->getData() << endl;
+    cout << lq.dequeue()->getData() << endl;
+
+    lq.display();
+
+    lq.enqueue(new Node(50));
+    lq.display();
 }
