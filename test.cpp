7f508a42d5a6e87edcb02898243c1d9aba3f57d1
jynoh00 <wndus123sh@naver.com>
2024-07-16 22:29:04 +0900

    cote_plus

--- test.cpp
+++ test.cpp
@@ -1,48 +1,40 @@
 #include <iostream>
-#include <set>
 #include <vector>
+#include <string>
+
 using namespace std;
 
+bool dfs(int i, int j, vector<vector<int>>& v, int N, int M){
+    if (i < 0 || i >= N || j < 0 || j >= M) return false;
+    if (v[i][j] == 0){
+        v[i][j] = 1;
+        dfs(i-1, j, v, N, M);
+        dfs(i+1, j, v, N, M);
+        dfs(i, j-1, v, N, M);
+        dfs(i, j+1, v, N, M);
+        return true;
+    }
+    return false;
+}
+
 int main(){
-    set<int> s;
-    s.insert(16);
-    s.insert(27);
-    s.insert(10);
-    s.insert(30);
+    int N, M;
+    int count = 0;
 
-    set<int>::iterator iter = s.emplace_hint(s.begin(), 101);
-    s.emplace_hint(iter, 1);
-
-    for (int i : s){cout << i << " ";}
-    cout << endl;
-
-    for (auto iter2 = s.begin(); iter2 != s.end(); iter2++){
-        cout << *iter2 << " ";
-    }
-    cout << endl;
-
-    for (auto iter2 = s.rbegin(); iter2 != s.rend(); iter2++){
-        cout << *iter2 << " ";
-    }
-    cout << endl;
-
-    set<int> s2(s);
-    s2.clear();
-    s2.emplace(3);
-    for(int i : s2){cout << i << " ";}
-    cout << endl;
+    cin >> N >> M;
+    vector<vector<int>> v(N, vector<int>(M));
     
-    s.swap(s2);
-    cout << "------" << endl;
-    for(int i : s){cout << i << " ";} cout << endl;
-    for(int i : s2){cout << i << " ";} cout << endl;
-
-    s2.erase(30);
-    for(int i : s2){cout << i << " ";} cout << endl;
-
-    set<int>::iterator iter3 = s2.find(27); // s.find(key) 찾는 Key값이 있으면, 해당 위치의 Iter 반환, 없으면 s.end()
-    set<int>::iterator iter4 = s2.find(999);
-    cout << *iter3 << endl;
-    cout << *iter4 << endl;
-    cout << *s2.end() << endl; // iter4와 s2.end() 동일
+    for (int i = 0; i < N; i++){
+        string input;
+        cin >> input;
+        for (int j = 0; j < M; j++){
+            v[i][j] = (input[j] - '0');
+        }
+    }
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < M; j++){
+            if (dfs(i, j, v, N, M)) count++;
+        }
+    }
+    cout << count << endl;
 }
