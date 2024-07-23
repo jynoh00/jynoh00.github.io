f0fd016afa1c8918604052a439bb09782bdcec4a
jynoh00 <wndus123sh@naver.com>
2024-07-23 21:23:25 +0900

    cpp_plus

--- test2.cpp
+++ test2.cpp
@@ -1,75 +1,92 @@
 #include <iostream>
-#include <vector>
-#include <set>
-#include <algorithm>
+#include <map>
+#include <unordered_map>
 
 using namespace std;
 
-int solution(vector<int> array){
-    set<int> s(array.begin(), array.end());
-    vector<int> vec(s.begin(), s.end());
-    vector<int> repeat(s.size());
-    int index = 0;
-    for (int i : s){
-        int r = 0;
-        for (int j : array){
-            if (j==i) r++; 
-        }
-        repeat[index] = r;
-        index++;
-    }
-    
-    int max_index;
-    int tmp = 0;
-    for (int i = 0; i < s.size(); i++){
-        if (repeat[i] > tmp){
-            tmp = repeat[i];
-            max_index = i;
-        }
-    }
-
-    sort(repeat.rbegin(), repeat.rend());
-    if (repeat[0] == repeat[1]) return -1;
-    else return vec[max_index];
-}
-// 쥐렸다
-
 int main(){
-    vector<int> v = {1, 2, 3,3,3,4};
-    vector<int> v1 = {1,1,2,2};
-    vector<int> v2 = {1,1,1,1,1,1, 2, 3,3,3,3, 5,5,5,5,5,5,5,5, 6,6,6,6,6};
+    //map<int, string> m1;
+    std::map<int, std::string> m2;
+    std::map<std::string, int> m3{
+        {"Kim", 23},
+        {"Lee", 24},
+        {"Noh", 25}
+    };
 
-    cout << solution(v) << endl;
+    std::cout << m3["Lee"] << std::endl; // 24
+    std::cout << m3["Noh"] << std::endl; // 25
+    m3["Noh"] = 20;
+    std::cout << m3["Noh"] << std::endl; // 20
+
+    m3["Park"] = 99; // insert, 없는 key 추가
+    std::cout << m3["Park"] << std::endl;
+
+    m3.insert(std::make_pair("Choi", 29)); // insert, using make_pair
+
+    m3.insert({"hel",52});
+
+    std::pair<std::string, int> p1{"Ho", 66};
+    m3.insert(p1);
+
+    if (m3.find("Ho") != m3.end()){
+        std::cout << "find" << std::endl;
+    }
+    else{
+        std::cout << "not find"<< std::endl;
+    }
+
+    std::cout << "-------" << std::endl;
+    for (std::map<std::string, int>::iterator iter = m3.begin(); iter != m3.end(); iter++){
+        std::cout << iter->first << " " << iter->second << std::endl;
+    }
+    std::cout << "-------" << std::endl;
+
+    std::cout << "-------" << std::endl;
+    for (auto iter : m3){ // auto 정리
+        std::cout << iter.first << " " << iter.second << std::endl;
+    }
+    std::cout << "-------" << std::endl;
+
+    // m3.erase(m3.begin()+2); // why err
+
+    m3.erase("Noh");
+
+    // clear all
+    m3.erase(m3.begin(), m3.end());
+    m3.clear();
+
+    unordered_map<int,int> m4;
+    m4.insert({2, 3});
+    cout << m4[2] << endl;
 }
 
-// tq
-// int solution(vector<int> array) {
-//     set<int> s(array.begin(), array.end());
-//     int arr[s.size()];
-//     int num = 0;
-//     for (int i : s){
-//         int a = 0;
-//         for (int j : array){
-//             if (j==i) a++;
-//         }
-//         arr[num] = a;
-//         num++;
-//     }
+/*
 
-//     vector<int> vec(s.begin(), s.end());
-//     int max_index = 0;
-//     int tmp = 0;
-//     for (int i = 0; i < s.size(); i++){
-//         if (arr[i] > tmp){
-//             tmp = vec[i];
-//             max_index = i;
-//         }
-//     }
+Associative container - map
 
-//     sort(arr, arr + s.size());
-//     if (arr[s.size()-2] == arr[s.size()-1]) return -1;
-//     else return vec[max_index];
-// }
+key -> data search can
+(python - ditionary와 유사)
+
+balanced binary tree, hashset
+연산 빠름
+
+map => #include <map> // 요소 정렬, 중복 허용 x
+unordered_map => #include <unordered_map> // 요소 정렬 x
+multimap => #include <map> // 중복된 요소 허용
+unordered_multimap => #include <unordered_map> // 요소 정렬 x, 중복된 요소 허용
+
+key - value 쌍으로 저장
+key 값에 따라 정렬
+key 중복 불가
+key로 직접 접근 가능
+반복자가 유효하지 않을 수 있다.
 
 
-// 최빈값 문제
+  key   value
+--------------
+| Kim  | 20  |
+--------------
+
+
+
+*/
