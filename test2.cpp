b93f5934a856787d876e5d4eaeb2dd26a50cc7de
jynoh00 <wndus123sh@naver.com>
2024-07-26 14:56:56 +0900

    ss plus

--- test2.cpp
+++ test2.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
-#include <vector>
+#include <string>
+#include <sstream>
+#include <map>
 
 using namespace std;
 
 int main(){
-    vector<long long> v(100);
-    v[0] = 1;
-    v[1] = 1;
-    int n = 6;
-    for (int i = 2; i < n; i++){
-        v[i] = v[i-1] + v[i-2];
+    map<string, string> m;
+    int n;
+    cin >> n;
+    cin.ignore(); // 
+
+    for (int i = 0; i < n; i++){
+        string s;
+        getline(cin, s);
+        stringstream ss(s);
+        string score, name;
+        ss >> name >> score;
+        m.insert({score, name});
     }
 
-    for (long long i : v){
-        if(i != 0) cout << i << " ";
-        }
+    for (auto iter : m){cout << iter.second << " ";}
     cout << endl;
+
+    return 0;
 }
